8d68108f9add149270d4c0e2da5f9ff61c539743
Alex <automex74@gmail.com>
2022-10-24 23:31:09 +0500

    add task_50

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 Console.WriteLine("--------------Задача №47.--------------");
 Console.WriteLine("Введите колличество строк: ");
-int rows = int.Parse(Console.ReadLine()!);
+int rows1 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите колличество столбцов: ");
-int columns = int.Parse(Console.ReadLine()!);
+int columns1 = int.Parse(Console.ReadLine()!);
 
-double[,] array = GetArray(rows, columns, -100, 100);
+double[,] array = GetArray(rows1, columns1, -100, 100);
 PrintArray(array);
 
 double[,] GetArray(int m, int n, int minValue, int maxValue)
@@ -33,3 +33,57 @@ void PrintArray(double[,] Array)
         Console.WriteLine();
     }
 }
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает
+// значение этого элемента или же указание, что такого элемента нет.
+Console.WriteLine("--------------Задача №50.--------------");
+int rows = 3;
+int columns = 4;
+int[,] arr = GetArray2(rows, columns, 0, 10);
+PrintArray2(arr);
+
+int[,] GetArray2(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray2(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("i = ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("j = ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a >= columns || b >= rows)
+{
+    Console.WriteLine("Такого числа в массиве нет.");
+}
+else
+{
+    rows = a; columns = b;
+    int n = arr[b,a];
+    Console.WriteLine(n);
+}
+
+
+
+
+
